b7dcefdc823311ce37664266ce1392fca4e1a530
James Burke <devnull@localhost>
2009-11-13 23:06:25 -0800

    Forgot main test file.

---
+++ tests/all.js
@@ -0,0 +1,5 @@
+doh.registerUrl("simple", "../simple.html");
+doh.registerUrl("circular", "../circular.html");
+doh.registerUrl("multiversion", "../multiversion.html", 10000);
+doh.registerUrl("i18n", "../i18n/i18n.html");
+doh.registerUrl("modifiers", "../modifier/modifier.html", 10000);
