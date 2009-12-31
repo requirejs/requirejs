da59a61a760ecbad64c0d913b3efa801e4f1b676
jrburke <jrburke@gmail.com>
2009-12-30 21:22:04 -0800

    Conversion of i18n bundles for dojo convert now works.

--- build/convert/convertDojo.js
+++ build/convert/convertDojo.js
@@ -39,6 +39,8 @@ var startTime = (new Date()).getTime(),
     jsFileRegExp = /\.js$/,
     depRegExp = /dojo\s*\.\s*(provide|require)\s*\(\s*["']([\w-_\.]+)["']\s*\)/g,
     reqRemoveRegExp = /dojo\s*\.\s*require\s*\(\s*["']([\w-_\.]+)["']\s*\)/g,
+    localeRegExp = /(^.*\/nls\/)([^\/]+)\/[^\/]+\.js$/,
+    rootBundleRegExp = /(^.*\/nls\/)([^\/]+)/,
     dojoJsRegExp = /\/dojo\.js(\.uncompressed\.js)?$/,
     fileName, convertedFileName, fileContents,
     i;
@@ -64,11 +66,13 @@ if (!fileList || !fileList.length) {
 } else {
     for (i = 0; (fileName = fileList[i]); i++) {
         convertedFileName = fileName.replace(dojoPath, savePath);
-        //Only do JS files and skip i18n bundles for now.
+        //Handle non-i18n JS files.
         if (jsFileRegExp.test(fileName) && fileName.indexOf("/nls/") === -1) {
             fileContents = fileUtil.readFile(fileName);
             fileContents = convert(fileName, fileContents);
             fileUtil.saveUtf8File(convertedFileName, fileContents);
+        } else if (fileName.indexOf("/nls/") !== -1) {
+            i18nConvert(fileName, convertedFileName, savePath);
         } else {
             //Just copy the file over.
             fileUtil.copyFile(fileName, convertedFileName, true);
@@ -92,13 +96,13 @@ convertTime = ((new Date().getTime() - startTime) / 1000);
 logger.info("Convert time: " + convertTime + " seconds");
 
 function writeRunEnd(prefixProps, contents) {
-    var reqString = "", argString = "", i, req;
+    var reqString = "", argString = "", i, req, getLocs = [], loc, varName;
 
     if (!prefixProps) {
         return contents;
     } else {
         //Convert dojo.cache references to be text! dependencies.
-        contents = contents.replace(/dojo\s*\.\s*cache\s*\(['"]([^'"]+)['"]\s*\,\s*['"]([^'"]+)['"]\s*\)/g, function(match, modName, fileName) {
+        contents = contents.replace(/dojo\s*\.\s*cache\s*\(['"]([^'"]+)['"]\s*\,\s*['"]([^'"]+)['"]\s*\)/g, function (match, modName, fileName) {
             var textName = "text!" + modName.replace(/\./g, "/") + "/" + fileName;
             //Make sure to use a bang for file extension part.
             textName = textName.split(".").join("!");
@@ -107,8 +111,42 @@ function writeRunEnd(prefixProps, contents) {
             return '_R' + (prefixProps.reqs.length - 1);
         });
 
+
+        //Convert dojo.requireLocalization calls to be i18n! dependencies.
+        contents = contents.replace(/dojo\s*\.\s*requireLocalization\s*\(['"]([^'"]+)['"]\s*\,\s*['"]([^'"]+)['"]\s*\)/g, function(match, prefix, baseName) {
+            var i18nName = "i18n!" + prefix.replace(/\./g, "/") + "/nls/" + baseName, varName = '_R';
+            prefixProps.reqs.push(i18nName);
+            varName += (prefixProps.reqs.length - 1);
+            
+            //Save off varName for getLocalization call replacements.
+            getLocs.push([prefix, baseName, varName]);
+            return "";
+        });
+
+        //If any getlLocalization calls were found, need to replace the getLocalization calls too.
+        if (getLocs.length) {
+            for (i = 0; (loc = getLocs[i]); i++) {
+                contents = contents.replace(new RegExp('dojo\\.i18n\\.getLocalization\\s*\\(\\s*([^,\\)]+)\\s*,\\s*([^,\\)]+)([^\\)]+)?\\)', 'g'),
+                                            function(match, prefix, baseName, thirdArg) {
+                                                var name = prefix + ' + "/nls/" + ' + baseName,
+                                                    getCall = 'run.get((' + name + ').replace(/\\./g, "/"))';
+                                                if (thirdArg) {
+                                                    //trim thirdArg, and remove an starting comma.
+                                                    thirdArg = thirdArg.replace(/\s*,\s*(.*)\s*$/, "$1");
+                                                    if (thirdArg) {
+                                                        return '(' + thirdArg + ' ? run.get((' + prefix + ' + "/nls/" + ' + thirdArg + ' + "/" + ' + baseName + ').replace(/\\./g, "/")) : ' + getCall + ')';
+                                                    } else {
+                                                        return getCall;
+                                                    }
+                                                } else {
+                                                    return getCall;
+                                                }
+                                            });
+            }
+        }
+
         //Build up the req string and args string.
-        for (i = 0; req = prefixProps.reqs[i]; i++) {
+        for (i = 0; (req = prefixProps.reqs[i]); i++) {
             reqString += ', "' + req + '"';
             argString += ', _R' + i;
         }
@@ -206,7 +244,7 @@ function convert(fileName, fileContents) {
                 reqs = [];
                 for (j = 0; (module = currentDep.requires[j]); j++) {
                     if (!deps.provides[module]) {
-                        reqs.push(module)
+                        reqs.push(module);
                     }
                 }
 
@@ -236,4 +274,42 @@ function convert(fileName, fileContents) {
     }
 
     return fileContents;
+}
+
+/**
+ * Converts i18n bundles. Takes a bit of work since we need to know the locales
+ * on the disk.
+ */
+function i18nConvert(fileName, convertedFileName, srcDir) {
+    var i, baseName, prefixName, matches, name, fileNames, locale, locales = "", text,
+        contents = fileUtil.readFile(fileName).replace(/^.*\(\{/m, "{").replace(/\)\s*$/, ""),
+        modName = convertedFileName.replace(srcDir, "").replace(/\.js$/, "");
+
+    if (modName.charAt(0) === "/") {
+        modName = modName.substring(1, modName.length);
+    }
+
+    if (localeRegExp.test(fileName)) {
+        //A locale-specific bundle. Easier to handle.
+        text = 'run("i18n!' + modName + '",\n' + contents + ');';
+    } else {
+        //A root bundle. A bit more work. First, get the basic name
+        matches = rootBundleRegExp.exec(modName);
+        prefixName = matches[1];
+        baseName = matches[2];
+        
+        //Find all files in that directory with same base name, so we can get
+        //the locales.
+        fileNames = fileUtil.getFilteredFileList(srcDir + "/" + prefixName, new RegExp(baseName + "\\.js$"), true);
+        for (i = 0; (name = fileNames[i]); i++) {
+            if ((matches = localeRegExp.exec(name))) {
+                locale = matches[2];
+                locales += ',\n"' + locale + '": "i18n!' + prefixName + locale + "/" + baseName + '"';
+            }
+        }
+
+        text = 'run("i18n!' + prefixName + baseName + '",\n{ "root": ' + contents + locales + '\n});';
+    }
+    
+    fileUtil.saveUtf8File(convertedFileName, text);
 }
