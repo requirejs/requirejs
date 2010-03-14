765b75d4fe3f76c4dff7c261b9fd4ab39465d73f
jrburke <jrburke@gmail.com>
2010-03-14 09:25:25 -0700

    More AST prepwork for build improvements.

--- build/parseTest.js
+++ build/parseTest.js
@@ -28,20 +28,60 @@ function closurefromCode(filename, content) {
 (function (args) {
     var buildHome = args[0],
         fileName = args[1],
+        //fileContents = 'require.def("foo", ["one", \n//This is a comment\n"two",\n/*Another comment*/"three"], {});',
+        //fileContents = 'require.def("foo", {one: "two"});',
         fileContents = readFile(fileName),
         rhino = Packages.com.google.javascript.rhino,
         jscomp = Packages.com.google.javascript.jscomp,
-        
-        EXPR_RESULT = 130,
-        CALL = 37,
+
+        //Values taken from com.google.javascript.rhino.Token,
+        //but duplicated here to avoid weird Java-to-JS transforms.
         GETPROP = 33,
+        CALL = 37,
         NAME = 38,
+        STRING = 40,
+        ARRAYLIT = 63,
+        OBJECTLIT = 64,
+        EXPR_RESULT = 130,
 
         //Set up source input
         jsSourceFile = closurefromCode(String(fileName), String(fileContents)),
         compiler = new jscomp.Compiler(),
         astRoot = compiler.parse(jsSourceFile),
-        i, node, call, require, def;
+        i, node, call, require, def, name, deps;
+
+    /**
+     * Calls node.getString() but makes sure a JS string is returned
+     */
+    function nodeString(node) {
+        return String(node.getString());
+    }
+
+    /**
+     * Validates a node as being an object literal (like for i18n bundles)
+     * or an array literal with just string members.
+     * This function does not need to worry about comments, they are not
+     * present in this AST.
+     */
+    function validateDeps(node) {
+        var type = node.getType(), i, dep;
+
+        if (type === OBJECTLIT) {
+            return true;
+        }
+
+        //Dependencies can be an object literal or an array. 
+        if (type !== ARRAYLIT) {
+            return false;
+        }
+
+        for (i = 0; (dep = node.getChildAtIndex(i)); i++) {
+            if (dep.getType() !== STRING) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     //Only look at top level calls to require() and require.def()?
     for (i = 0; (node = astRoot.getChildAtIndex(i)); i++) {
@@ -49,8 +89,8 @@ function closurefromCode(filename, content) {
             node.getFirstChild().getType() === CALL &&
             node.getFirstChild().getFirstChild().getType() === GETPROP &&
             node.getFirstChild().getFirstChild().getFirstChild().getType() === NAME &&
-            node.getFirstChild().getFirstChild().getFirstChild().getString() + "" === "require") {
-            
+            nodeString(node.getFirstChild().getFirstChild().getFirstChild()) === "require") {
+
             //call.getChildAt(1) should give first arg to the call.
             call = node.getFirstChild();
             require = call.getFirstChild().getChildAtIndex(0);
@@ -59,9 +99,27 @@ function closurefromCode(filename, content) {
             if (!def) {
                 //It is a plain require() call.
                 logger.trace("found a require(), first arg is: " + call.getChildAtIndex(1));
-            } else if (def.getString() + "" === "def") {
+                deps = call.getChildAtIndex(1);
+                if (!validateDeps(deps)) {
+                    continue;
+                }
+
+            } else if (nodeString(def) === "def") {
                 //A require.def call
+                name = call.getChildAtIndex(1);
+                deps = call.getChildAtIndex(2);
                 logger.trace("found a def, first arg is: " + call.getChildAtIndex(1));
+
+                //Validate def name as a string
+                if (name.getType() !== STRING) {
+                    continue;
+                }
+                if (!validateDeps(deps)) {
+                    continue;
+                }
+                logger.trace("VALID DEF, first arg is: " + call.getChildAtIndex(1));
+
+                
             } else {
                 //Some other require call, like require.pause, require.ready
                 //that we do not want to track.
--- build/parsedebug.sh
+++ build/parsedebug.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 MYDIR=$(cd $(dirname "$0"); pwd)
-# java -classpath $MYDIR/lib/rhino/js.jar:$MYDIR/lib/closure/compiler.jar org.mozilla.javascript.tools.debugger.Main $MYDIR/parseTest.js $MYDIR "$@"
+#java -classpath $MYDIR/lib/rhino/js.jar:$MYDIR/lib/closure/compiler.jar org.mozilla.javascript.tools.debugger.Main $MYDIR/parseTest.js $MYDIR "$@"
 
 java -classpath $MYDIR/lib/rhino/js.jar:$MYDIR/lib/closure/compiler.jar org.mozilla.javascript.tools.shell.Main $MYDIR/parseTest.js $MYDIR "$@"
