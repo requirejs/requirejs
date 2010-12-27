613ea8e97c37bb527b8853afb121eba47d71a41e
jrburke <jrburke@gmail.com>
2010-12-27 15:55:36 -0800

    Fix a path normalization issue with .., where a .. was not normalized out in a legitimate case: ui/../util/foo.

--- build/convert/node/canvasTest.js
+++ build/convert/node/canvasTest.js
@@ -2,13 +2,30 @@
 //npm install canvas
 //npm install express
 
+'use strict';
+/*jslint */
+/*global require: false, console: false */
+
 var Canvas = require('canvas'),
     express = require('express'),
-    canvas = new Canvas(200,200),
-    ctx = canvas.getContext('2d');
+    canvas = new Canvas(200, 200),
+    ctx = canvas.getContext('2d'),
+    app = express.createServer();
+
+
+app.configure(function () {
+
+    app.get('/', function (req, res) {
+        res.send('hello world');
+    });
+
+    //app.use(express.gzip());
+    app.listen(3000);
+});
+
 
 ctx.font = '30px Impact';
-ctx.rotate(.1);
+ctx.rotate(0.1);
 ctx.fillText("Awesome!", 50, 100);
 
 var te = ctx.measureText('Awesome!');
@@ -19,4 +36,3 @@ ctx.lineTo(50 + te.width, 102);
 ctx.stroke();
 
 console.log('<img src="' + canvas.toDataURL() + '" />');
-
--- require.js
+++ require.js
@@ -158,13 +158,15 @@ var require, define;
                     ary.splice(i, 1);
                     i -= 1;
                 } else if (part === "..") {
-                    if (i === 1) {
+                    if (i === 1 && ary[2] === '..') {
                         //End of the line. Keep at least one non-dot
                         //path segment at the front so it can be mapped
                         //correctly to disk. Otherwise, there is likely
-                        //no path mapping for '..'.
+                        //no path mapping for a path starting with '..'.
+                        //This can still fail, but catches the most reasonable
+                        //uses of ..
                         break;
-                    } else if (i > 1) {
+                    } else if (i > 0) {
                         ary.splice(i - 1, 2);
                         i -= 2;
                     }
