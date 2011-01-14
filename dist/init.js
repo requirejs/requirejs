4e75ebc4d462c52f06b837a12f3668d849639a15
jrburke <jrburke@gmail.com>
2011-01-14 15:47:21 -0800

    some site tweaks, task update.

--- dist/init.js
+++ dist/init.js
@@ -1,14 +1,14 @@
 $(document).ready(function($) {
 
-    // set panel min-height to full browser height   
+    // set panel min-height to full browser height
     $(window).bind("load resize", function(){
         var h = $(window).height();
         $("#wrapper, #grid, #navBg").css({ "min-height" : (h) });
-        $("#content").css({ "min-height" : (h-147) });
+        $("#content").css({ "min-height" : (h-168) });
     });
-    
+
     // toggle grid
-    
+
     $("#grid").hide();
 
     $(document.documentElement).keyup(function (event) {
@@ -16,7 +16,7 @@ $(document).ready(function($) {
             $("#grid").fadeToggle(100);
         }
     });
-    
+
     // if window is larger than #nav then #nav == fixed, if #nav is larger than window #nav == relative
     $(window).bind("load resize", function(){
         var w = jQuery(window).height();
@@ -25,4 +25,4 @@ $(document).ready(function($) {
     });
 
     // done!
-});  
+});
