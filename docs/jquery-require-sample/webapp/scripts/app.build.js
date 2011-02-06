cc0c64d2aa06d65fe12a6064b4fcffe6379a5b5d
jrburke <jrburke@gmail.com>
2011-02-05 20:55:27 -0800

    Add GCF meta tag, update baseUrl refs in docs to not include slash, document that JSONP support is restricted to objects only. Closes #60

--- docs/jquery-require-sample/webapp/scripts/app.build.js
+++ docs/jquery-require-sample/webapp/scripts/app.build.js
@@ -1,6 +1,6 @@
 ({
     appDir: "../",
-    baseUrl: "scripts/",
+    baseUrl: "scripts",
     dir: "../../webapp-build",
     //Comment out the optimize line if you want
     //the code minified by Closure Compiler using
