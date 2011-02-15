5f9198978eaebfcffb18f9676abe7a892f522046
jrburke <jrburke@gmail.com>
2011-02-14 23:58:38 -0800

    updating version number/docs in preparation of a release.

--- require.js
+++ require.js
@@ -1,5 +1,5 @@
 /** vim: et:ts=4:sw=4:sts=4
- * @license RequireJS 0.22.0+ Copyright (c) 2010-2011, The Dojo Foundation All Rights Reserved.
+ * @license RequireJS 0.23.0 Copyright (c) 2010-2011, The Dojo Foundation All Rights Reserved.
  * Available via the MIT or new BSD license.
  * see: http://github.com/jrburke/requirejs for details
  */
@@ -11,7 +11,7 @@
 var require, define;
 (function () {
     //Change this version number for each release.
-    var version = "0.22.0+",
+    var version = "0.23.0",
         commentRegExp = /(\/\*([\s\S]*?)\*\/|\/\/(.*)$)/mg,
         cjsRequireRegExp = /require\(["']([^'"\s]+)["']\)/g,
         currDirRegExp = /^\.\//,
