ebb10cf6ac5d703f26137b725ceb3078ccc29b09
jrburke <jrburke@gmail.com>
2012-04-26 21:53:08 -0700

    built legacy test files.

---
+++ tests/legacy/built/basic-tests.js
@@ -0,0 +1,74 @@
+
+(function (root) {
+    root.A = {
+        name: 'a'
+    };
+}(this));
+
+define("a", (function (global) {
+    return function () {
+        var func = function (){return this.A.name};
+        return func.apply(global, arguments);
+    }
+}(this)));
+
+function D() {
+    this.name = 'd';
+};
+
+define("d", function(){});
+
+var B = {
+    name: 'b',
+    aValue: A.name,
+    dValue: new D()
+};
+
+define("b", function(){});
+
+var C = {
+    name: 'c',
+    a: A,
+    b: B
+};
+
+define("c", ["a","b"], (function (global) {
+    return function () {
+        return global["C"];
+    }
+}(this)));
+
+require({
+        baseUrl: './',
+        legacy: {
+            a: {
+                exports: function () {
+                    return this.A.name;
+                }
+            },
+            'b': ['a', 'd'],
+            'c': {
+                deps: ['a', 'b'],
+                exports: 'C'
+            }
+        }
+    },
+    ['a', 'c'],
+    function(a, c) {
+        doh.register(
+            'legacyBasic',
+            [
+                function legacyBasic(t){
+                    t.is('a', a);
+                    t.is('a', c.b.aValue);
+                    t.is('b', c.b.name);
+                    t.is('c', c.name);
+                    t.is('d', c.b.dValue.name);
+                }
+            ]
+        );
+        doh.run();
+    }
+);
+
+define("basic-tests", function(){});
