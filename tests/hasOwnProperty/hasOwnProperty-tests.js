d7ac9fb5a4483fa5ff1b9510fbb41d95ff8d0960
jrburke <jrburke@gmail.com>
2012-11-21 12:05:35 -0800

    Refs #525, add prototype to the test.

--- tests/hasOwnProperty/hasOwnProperty-tests.js
+++ tests/hasOwnProperty/hasOwnProperty-tests.js
@@ -1,10 +1,11 @@
-require(["toString", "hasOwnProperty"], function(toString, hop) {
+require(["toString", "hasOwnProperty", "prototype"], function(toString, hop, p) {
         doh.register(
             "hasOwnPropertyTests",
             [
                 function hasOwnPropertyTests(t){
                     t.is("toString", toString.name);
-                    t.is("hasOwnProperty", hop.name)
+                    t.is("hasOwnProperty", hop.name);
+                    t.is("prototype", p.name);
                 }
             ]
         );
