704768fa030a0f986dee4365c60dfa97bc113147
Kevin Ridgway <kridgway@gmail.com>
2013-04-12 00:58:58 -0300

    Fixed misspelling.
    :)

--- require.js
+++ require.js
@@ -1605,7 +1605,7 @@ var requirejs, require, define;
             },
 
             /**
-             * Executes a module callack function. Broken out as a separate function
+             * Executes a module callback function. Broken out as a separate function
              * solely to allow the build system to sequence the files in the built
              * layer in the right sequence.
              *
