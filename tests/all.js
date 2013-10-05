8fe35570d78d6911aab0155c27e43957bfba7e5e
jrburke <jrburke@gmail.com>
2013-10-05 16:50:38 -0700

    add new test to list of tests

--- tests/all.js
+++ tests/all.js
@@ -15,6 +15,7 @@ doh.registerUrl("urlArgsToUrl", "../urlArgsToUrl.html");
 doh.registerUrl("config", "../config.html");
 doh.registerUrl("configRequirejs", "../configRequirejs.html");
 doh.registerUrl("dataMain", "../dataMain/dataMain.html");
+doh.registerUrl("skipDataMain", "../dataMain/skipDataMain/skipDataMain.html");
 doh.registerUrl("dataMainIndex", "../dataMain/dataMainIndex/dataMainIndex.html");
 doh.registerUrl("dataMainBaseUrl", "../dataMain/baseUrl/dataMainBaseUrl.html");
 
