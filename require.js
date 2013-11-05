91dd5fb4a1a8b3dafeca1a53442b3b0127b3e7a2
LukeLin <773000785@qq.com>
2013-11-06 00:11:20 +0800

    Update require.js

--- require.js
+++ require.js
@@ -13,8 +13,7 @@ var requirejs, require, define;
     var req, s, head, baseElement, dataMain, src,
         interactiveScript, currentlyAddingScript, mainScript, subPath,
         version = '2.1.4+',
-        // In case of the first "/" escaped
-        commentRegExp = /(\/\*([\s\S]*?)\*\/|([^:\\]|^)\/\/(.*)$)/mg;
+        commentRegExp = /(\/\*([\s\S]*?)\*\/|([^:]|^)\/\/(.*)$)/mg,
         cjsRequireRegExp = /[^.]\s*require\s*\(\s*["']([^'"\s]+)["']\s*\)/g,
         jsSuffixRegExp = /\.js$/,
         currDirRegExp = /^\.\//,
