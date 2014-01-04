3cf985738a91a62e4f56811754cf56ac2a5bdccc
jrburke <jrburke@gmail.com>
2014-01-04 00:15:49 -0800

    Relates to #980, wiring up test

--- tests/moduleExports/program.js
+++
@@ -1,5 +0,0 @@
-define(["module", "exports", "test"], function(module, exports, test) {
-test.assert(module.exports === exports, 'exports equals module.exports');
-test.print('DONE', 'info');
-
-});
