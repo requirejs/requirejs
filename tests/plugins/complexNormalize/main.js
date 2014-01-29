b3d5fbc0c10a1ab12e1b9f324367a2bc2b7daa3c
jrburke <jrburke@gmail.com>
2014-01-28 16:30:11 -0800

    Relates to #1000, moving around the test

--- tests/plugins/complexNormalize/main.js
+++ tests/plugins/complexNormalize/main.js
@@ -1,8 +1,8 @@
-define(['sub/modA1', 'sub/plugin!modB1'], function (modAName, modBName) {
+require(['sub/modA1', 'sub/plugin!modB1'], function (modAName, modBName) {
     doh.register(
-        'complexNormalize',
+        'pluginComplexNormalize',
         [
-            function complexNormalize(t){
+            function pluginComplexNormalize(t){
                 t.is(true, /normalized/i.test(modAName));
                 t.is(true, /normalized/i.test(modBName));
             }
