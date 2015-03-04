b1b066cfc25f8eee685cbba6a300dc4e3e2448a0
savajolssylvain <s.savajols@gmail.com>
2015-03-04 19:30:56 +0100

    Add contextName to localRequire

--- require.js
+++ require.js
@@ -1441,6 +1441,8 @@ var requirejs, require, define;
                 mixin(localRequire, {
                     isBrowser: isBrowser,
 
+                    contextName: this.contextName,
+
                     /**
                      * Converts a module name + .extension into an URL path.
                      * *Requires* the use of a module name. It does not support using
