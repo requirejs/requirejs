2b804c55b45b2a5eedf43100b02eb41cc7a4dff8
trazyn <tn.razy@gmail.com>
2015-04-15 11:44:08 +0800

    fixfox cann't resolve the '\'

--- require.js
+++ require.js
@@ -1989,6 +1989,8 @@ var requirejs, require, define;
                     mainScript = dataMain;
                 }
 
+                dataMain = dataMain.replace( "\\", "/" );
+
                 //Put the data-main script in the files to load.
                 cfg.deps = cfg.deps ? cfg.deps.concat(mainScript) : [mainScript];
 
