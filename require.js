ce35530123cda24efc2477cbf62211016dbf9c15
chaomaochina88@163.com <chaomaochina88@163.com>
2016-05-19 16:43:42 +0800

    修复资源引用BUG

--- require.js
+++ require.js
@@ -12,7 +12,7 @@ var requirejs, require, define;
     var req, s, head, baseElement, dataMain, src,
         interactiveScript, currentlyAddingScript, mainScript, subPath,
         version = '2.2.0',
-        commentRegExp = /(\/\*([\s\S]*?)\*\/|([^:]|^)\/\/(.*)$)/mg,
+        commentRegExp = /(\/\*([\s\S]*?)\*\/|([^:'"=]|^)\/\/(.*)$)/mg,
         cjsRequireRegExp = /[^.]\s*require\s*\(\s*["']([^'"\s]+)["']\s*\)/g,
         jsSuffixRegExp = /\.js$/,
         currDirRegExp = /^\.\//,
